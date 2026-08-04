30f44a19a0e56c1f7d8fb808616a0cc8238dbabd
seegeneroso <sergiofgeneroso@hotmail.com>
2026-08-04 14:54:51 -0300

    Geração inicial do projeto spring

---
+++ src/main/java/com/seegeneroso/gestao_custos_obras/GestaoCustosObrasApplication.java
@@ -0,0 +1,13 @@
+package com.seegeneroso.gestao_custos_obras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoCustosObrasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoCustosObrasApplication.class, args);
+	}
+
+}
